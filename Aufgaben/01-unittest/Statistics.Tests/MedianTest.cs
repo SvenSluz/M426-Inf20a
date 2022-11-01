6e7edc1258b151494fe87bcf81765840058878b6
Sven <sven_lai@sluz.ch>
2022-11-01 20:34:56 +0100

    Aufgabe 5.1: Testfälle für Median erstellt. Compiliert, aber die Tests failen.

---
+++ Aufgaben/01-unittest/Statistics.Tests/MedianTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Statistics.Tests;
+
+public class MedianTest
+{
+    [Fact]
+    public void TestMedianForOddNumberOfElements()
+    {
+        //given
+        var numbers = new List<double> {1, 2, 3, 4, 5};
+        var expected = 3.0;
+        var median = new Median();
+        //when
+        var actual = median.CalculateMedian(numbers);
+        //then
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void TestMedianForEvenNumberOfElements()
+    {
+        //given
+        var numbers = new List<double> {1, 2, 4, 5};
+        var expected = 3.0;
+        var median = new Median();
+        //when
+        var actual = median.CalculateMedian(numbers);
+        //then
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void TestMedianForNoElements()
+    {
+        //given
+        var numbers = new List<double>();
+        var median = new Median();
+        //when
+        //then
+        Assert.Throws<ArgumentException>(()=>median.CalculateMedian(numbers));
+    }
+}
---
+++ Aufgaben/01-unittest/Statistics/Median.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Statistics;
+
+public class Median
+{
+    public double CalculateMedian(List<double> numbers)
+    {
+        throw new System.NotImplementedException();
+    }
+}
