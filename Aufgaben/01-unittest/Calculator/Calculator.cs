e932f1e67a74406277ec56bdeca3033499309f5e
Sven <sven_lai@sluz.ch>
2022-11-01 19:19:02 +0100

    Aufgabe 1.3: Produktivcode so manipuliert, dass die Tests failen

--- Aufgaben/01-unittest/Calculator/Calculator.cs
+++ Aufgaben/01-unittest/Calculator/Calculator.cs
@@ -6,12 +6,12 @@ namespace Calculator
     {
         public int Add(int summandA, int summandB)
         {
-            return summandA + summandB;
+            return summandA + summandB + 1;
         }
 
         public int Subtract(int minuend, int subtrahend)
         {
-            return minuend - subtrahend;
+            return minuend - subtrahend + 1;
         }
     }
 }
