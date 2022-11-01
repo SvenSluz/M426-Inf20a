f180dec78d5d87c45fd5ef222fdcf74a3cf331aa
Sven <sven_lai@sluz.ch>
2022-11-01 20:16:58 +0100

    Aufgabe 4.2 TestMeanOfFiveNumbers hinzugefügt.

--- Aufgaben/01-unittest/Statistics.Tests/AverageTest.cs
+++ Aufgaben/01-unittest/Statistics.Tests/AverageTest.cs
@@ -17,5 +17,17 @@ namespace Statistics.Tests
 
             Assert.Equal(expected, actual);
         }
+
+        [Fact]
+        public void TestMeanOfFiveNumbers()
+        {
+            List<int> numbers = new List<int> {5, 10, 15, 20, 25};
+            double expected = 15;
+            Average average = new Average();
+
+            double actual = average.Mean(numbers);
+
+            Assert.Equal(expected, actual);
+        }
     }
 }
