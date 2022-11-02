5e6828d6a7e3e671ebda5bc6a86a9ccb24b1d291
Sven Lai <sven_lai@sluz.ch>
2022-11-02 13:25:08 +0100

    AufgabeFooBarQix: Testfälle und Methode Determine erstellt. Testfälle laufen alle erfolgreich durch.

--- Aufgaben/02-tdd/FooBarQix.Tests/FooBarQixDeterminerTest.cs
+++ Aufgaben/02-tdd/FooBarQix.Tests/FooBarQixDeterminerTest.cs
@@ -5,8 +5,115 @@ namespace FooBarQix.Tests
     public class FooBarQixDeterminerTest
     {
         [Fact]
-        public void Test1()
+        public void Determine_WhenNumberNotDivisible_ReturnNumberAsText()
         {
+            //arrange
+            int number = 1;
+            var fooBarQixDeterminer = new FooBarQixDeterminer();
+            var expected = number.ToString();
+            //act
+            string actual = FooBarQixDeterminer.Determine(number);
+
+            //assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Determine_WhenNumberDivisibleByOnlyThree_ReturnFoo()
+        {
+            //arrange
+            int number = 6;
+            var fooBarQixDeterminer = new FooBarQixDeterminer();
+            var expected = "Foo";
+            //act
+            string actual = FooBarQixDeterminer.Determine(number);
+
+            //assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Determine_WhenNumberDivisibleByOnlyFive_ReturnBar()
+        {
+            //arrange
+            int number = 10;
+            var fooBarQixDeterminer = new FooBarQixDeterminer();
+            var expected = "Bar";
+            //act
+            string actual = FooBarQixDeterminer.Determine(number);
+
+            //assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Determine_WhenNumberDivisibleByOnlySeven_ReturnBar()
+        {
+            //arrange
+            int number = 14;
+            var fooBarQixDeterminer = new FooBarQixDeterminer();
+            var expected = "Qix";
+            //act
+            string actual = FooBarQixDeterminer.Determine(number);
+
+            //assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Determine_WhenNumberDivisibleByThreeAndFive_ReturnFooBar()
+        {
+            //arrange
+            int number = 15;
+            var fooBarQixDeterminer = new FooBarQixDeterminer();
+            var expected = "FooBar";
+            //act
+            string actual = FooBarQixDeterminer.Determine(number);
+
+            //assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Determine_WhenNumberDivisibleByThreeAndSeven_ReturnFooBar()
+        {
+            //arrange
+            int number = 21;
+            var fooBarQixDeterminer = new FooBarQixDeterminer();
+            var expected = "FooQix";
+            //act
+            string actual = FooBarQixDeterminer.Determine(number);
+
+            //assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Determine_WhenNumberDivisibleByFiveAndSeven_ReturnBarQix()
+        {
+            //arrange
+            int number = 35;
+            var fooBarQixDeterminer = new FooBarQixDeterminer();
+            var expected = "BarQix";
+            //act
+            string actual = FooBarQixDeterminer.Determine(number);
+
+            //assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Determine_WhenNumberDivisibleByThreeFiveAndSeven_ReturnFooBarQix()
+        {
+            //arrange
+            int number = 105;
+            var fooBarQixDeterminer = new FooBarQixDeterminer();
+            var expected = "FooBarQix";
+            //act
+            var actual = FooBarQixDeterminer.Determine(number);
+
+            //assert
+            Assert.Equal(expected, actual);
         }
     }
 }
--- Aufgaben/02-tdd/FooBarQix/FooBarQixDeterminer.cs
+++ Aufgaben/02-tdd/FooBarQix/FooBarQixDeterminer.cs
@@ -2,6 +2,12 @@
 {
     public class FooBarQixDeterminer
     {
+        public static string Determine(int input)
+        {
+            var result = ((input % 3 == 0) ? "Foo" : "") + ((input % 5 == 0) ? "Bar" : "") +
+                         ((input % 7 == 0) ? "Qix" : "");
 
+            return string.IsNullOrEmpty(result) ? input.ToString() : result;
+        }
     }
 }
